64499db446f24374e9ca93824418b985c01ec04c
Dave Winniford <dwinniford@gmail.com>
2020-02-27 16:53:06 -0600

    creates users table

--- Gemfile
+++ Gemfile
@@ -6,8 +6,8 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 gem 'sinatra'
-gem 'sqlite3'
-gem 'activerecord', :require => "active_record"
+gem 'sqlite3', '~> 1.3.6'
+gem 'activerecord', '5.2', :require => "active_record"
 gem 'rake'
 gem 'pry'
 gem 'sinatra-activerecord'
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,14 @@
 require 'bundler'
 
-Bundler.require
+Bundler.require#(:default, ENV['SINATRA_ENV'])  
 
 ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
     :database => "db/development.sqlite"
 )
+
+# configure :development do
+# 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
+# end
 
 require_all 'app'
