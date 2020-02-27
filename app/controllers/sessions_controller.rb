cfb8aa96897ba6f617cdd65647068ab00caf9d85
Dave Winniford <dwinniford@gmail.com>
2020-02-27 15:16:15 -0600

    verifies login user_name is not empty

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,15 @@ class SessionsController < ApplicationController
 
     post '/sessions' do 
         session[:user_name] = params[:user_name]
-        redirect "/users" # add user id in the uri
+        if session[:user_name].length > 0  # add real authentication
+            redirect "/users"
+        else 
+            redirect "/login" # add user id in the uri
+        end 
+    end 
+
+    get '/logout' do 
+        sessions.clear 
+        redirect :"/sessions/login"
     end 
 end 
