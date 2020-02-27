0798e1c007aac4f4d174554db07c65f95bc49fba
Dave Winniford <dwinniford@gmail.com>
2020-02-27 14:58:16 -0600

    adds landing page for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,9 @@ class SessionsController < ApplicationController
     get '/login' do 
         erb :'sessions/login'
     end 
+
+    post '/sessions' do 
+        session[:user_name] = params[:user_name]
+        redirect "/users" # add user id in the uri
+    end 
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
 class UsersController < ApplicationController
-   
+   get "/users" do  # change to /users/:id after add db
+    @user_name = session[:user_name]
+    erb :"users/show"
+   end 
 end 
