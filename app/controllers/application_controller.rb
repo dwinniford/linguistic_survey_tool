3165a67e3e13d11b7284b62fead543f935fe94ac
Dave Winniford <dwinniford@gmail.com>
2020-02-27 15:23:34 -0600

    adds helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,16 @@ class ApplicationController < Sinatra::Base
     get '/' do 
         "hello world"
     end 
+
+    helpers do 
+        
+        def current_user
+            User.find(session[:user_id])
+        end 
+
+        def logged_in?
+            !!session[:user_id]
+        end 
+    end 
     
 end 
