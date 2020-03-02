244db2bbe36c968f1a7a3d6ec3ab0f20f6777b6c
Dave Winniford <dwinniford@gmail.com>
2020-03-02 17:03:41 -0600

    add logout and new survey links to surveys index

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,4 +5,8 @@ class SurveysController < ApplicationController
         @user = User.find_by_id(session[:id])
         erb :"/surveys/index"
     end 
+
+    get '/surveys/new' do 
+        erb :"/surveys/new"
+    end 
 end 
