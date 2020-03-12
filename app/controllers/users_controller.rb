cd870b9059c7ac07f967e8024cda3685f75f2123
Dave Winniford <dwinniford@gmail.com>
2020-03-12 14:04:19 -0500

    adds home page for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,8 @@
 class UsersController < ApplicationController
-   
+   get '/users/home' do 
+        @user = current_user
+        erb :'/users/home'
+   end
 end 
+
+#create a show route for a user and on that page show me each of their survey names and location 
